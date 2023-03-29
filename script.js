f1a2b41f81c9f7ba03029be14fa6f2947a96896f
SidnyZH\Sidny <syzapataho@gmail.com>
2023-03-29 16:13:06 -0500

    Add a task in the first place

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ const btn= document.querySelector("[data-form-btn]");
 
 //(arrow function o función anónima)
 const createTask = (evento)  => {
-    evento.preventDefault; 
+    evento.preventDefault(); 
     const input = document.querySelector("[data-form-input]")
     const value = input.value;
-    input.value = "";
-    console.log(value)};
+    const task = document.querySelector("[data-task]")
+    input.value = "";  
+    //Backticks  
+    const content = ` <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i></li>`;
+   task.innerHTML = content;   
+    console.log(content)
+};
 
 // listener. fuincion  que recibe la accion que quiero escuchar, que quiero que pase cuando el evento escuche esta acción 
 btn.addEventListener("click", createTask);
