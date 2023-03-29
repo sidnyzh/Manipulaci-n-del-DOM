0f70161c547f74f47bb0265b3a83971742355e24
SidnyZH\Sidny <syzapataho@gmail.com>
2023-03-29 15:59:49 -0500

    The input content clears after click on "agregar"

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const btn= document.querySelector("[data-form-btn]");
 const createTask = (evento)  => {
     evento.preventDefault; 
     const input = document.querySelector("[data-form-input]")
-    console.log(input.value)};
+    const value = input.value;
+    input.value = "";
+    console.log(value)};
 
 // listener. fuincion  que recibe la accion que quiero escuchar, que quiero que pase cuando el evento escuche esta acción 
 btn.addEventListener("click", createTask);
